0cc077a651ba0c2fedfbd670cd13ba7865c19a92
Roma Knyaz <git@knyaz.tech>
2022-07-22 00:14:51 +0200

    Sorting in correct order

--- demo/main.cpp
+++ demo/main.cpp
@@ -12,7 +12,7 @@ using namespace graphics;
 using std::cout, std::endl;
 
 #define eps 1e-6
-constexpr double N = 100;
+constexpr double N = 80;
 constexpr double Nh = N / 2;
 
 int main(int argc, char* args[]) {
@@ -30,16 +30,22 @@ int main(int argc, char* args[]) {
     OctreeRoot world;
     for (int i = 0; i < 14; i++)
         world.grow();
-    world.fill({0, 0, 10}, 0, GREEN);
-    world.fill({4, 0, 10}, 0, BLUE);
+    for (int i = 8; i < 1000; i+= 8) {
+        world.fill({0, 0, 8 + i}, 2, GREEN);
+        world.fill({0, 0, 12 + i}, 2, BLUE);
+    }
 
 
     // Main render cycle
     while (renderer.update()) {
         renderer.clear(GRAY);
 
+        ll perfCounterTotal = 0, perfCounterFilled = 0, filledPixels = 0;
+
         for (int xi = 0; xi < N; xi++) {
             for (int yi = 0; yi < N; yi++) {
+                ll perfCounter = 0;
+
                 AlignedRect rect({static_cast<double>(xi - Nh) / Nh, static_cast<double>(yi - Nh) / Nh},
                                  {static_cast<double>((xi + 1) - Nh) / Nh, static_cast<double>((yi + 1) - Nh) / Nh });
 
@@ -79,12 +85,30 @@ int main(int argc, char* args[]) {
                     // if node is semi - continue iteration, push all the children sorted by the distance to node
                     // for each node check that it intersects with current beam (thick ray), and if so - push it to stack
                     Cube rootCube = world.getCubeFor(node);
-                    for (size_t i = 0; i < 8; i++) { // TODO: make for each cycle for Triplet
-                        OctreeBase* child = node->getChild(i);
-                        Cube cube = Octree::getCubeForChild(rootCube, i);
 
+                    using S_T = std::pair<std::pair<OctreeBase*, Cube>, double>;
+                    std::vector<S_T> sorted;
+                    for (size_t i = 0; i < 8; i++) {
+                        OctreeBase* child = node->getChild(i);
                         if (child->isEmpty())
                             continue;
+
+                        Cube cube = Octree::getCubeForChild(rootCube, i);
+                        double distance = (Vec3f(cube.pos) - cam.getPosition()).size();
+
+                        sorted.push_back({{child, cube}, distance});
+                    }
+
+                    std::sort(sorted.begin(), sorted.end(), [](const S_T& A, const S_T &B) {
+                       return A.second < B.second;
+                    });
+
+                    for (auto zipped : sorted) {
+
+                        perfCounter++;
+
+                        auto *child = zipped.first.first;
+                        Cube cube = zipped.first.second;
 
                         auto *projected = new Vec2f[8];
                         size_t pointsNumber = 0;
@@ -101,20 +125,29 @@ int main(int argc, char* args[]) {
 
                         AlignedRect box(projected, pointsNumber);
 
-                        // TODO: add bounding box test
-                        if (rect.intersects(box) && GJK::GJK(rect, Polygon(projected, pointsNumber)))
+                        if (rect.intersects(box) && GJK::GJK(rect, Polygon(projected, pointsNumber))) {
                             stack.push(child);
+                            break;
+                        }
                     }
                 }
 
+                perfCounterTotal += perfCounter;
                 if (fill) {
                     Vec2f mid = rect.min;
                     Vec2f dim = rect.max - rect.min;
-                    auto tr = [](double x) {return x * 400 + 400;};
+                    auto tr = [](double x) {return round(x * 400 + 400);};
                     renderer.drawRect(Rect(tr(mid.x), tr(mid.y), 800 / N, 800 / N), color);
+
+                    perfCounterFilled += perfCounter;
+                    filledPixels += 1;
                 }
             }
         }
+
+        std::cout << "total iterations: " << perfCounterTotal << std::endl;
+        std::cout << "iterations per filled: " <<
+            (filledPixels == 0 ? 0 : static_cast<double>(perfCounterFilled / filledPixels)) << std::endl;
 
         // position controls
         if (renderer.pressed['j'])
