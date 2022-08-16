2a5d4014ddf9895a049ca2fbf7cbfe19efd12fcd
Roma Knyaz <git@knyaz.tech>
2022-08-16 14:40:20 +0200

    Change some ops names

--- core/include/BasicMath.h
+++ core/include/BasicMath.h
@@ -6,12 +6,12 @@
 #define SABT_BASICMATH_H
 
 template <typename T1, typename T2>
-T1 max(T1 a, T2 b) {
+T1 max2(T1 a, T2 b) {
     return a > b ? a : b;
 }
 
 template <typename T1, typename T2>
-T1 min(T1 a, T2 b) {
+T1 min2(T1 a, T2 b) {
     return a < b ? a : b;
 }
 
